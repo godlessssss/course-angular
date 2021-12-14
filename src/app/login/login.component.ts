7aa7e8d9e0f638f1d1aeb2f6560ec3b8c8598285
godlessssss <48207266+godlessssss@users.noreply.github.com>
2021-12-14 21:21:33 +0100

    feat: init + login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { FormControl, FormGroup } from '@angular/forms'
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  })
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+    let res = this.http.post('http://localhost:3000/auth/login', {
+      username: this.loginForm.value.username,
+      password: this.loginForm.value.password
+    })
+
+    res.subscribe((data: any) => {
+      sessionStorage.setItem('access_token', data.token)
+    })
+  }
+
+}
